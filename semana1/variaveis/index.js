c5e6eccb7046dee95dab81dec05c44522ef9fcec
GuilhermeMedici <85452389+GuilhermeMedici@users.noreply.github.com>
2021-06-14 15:56:48 -0400

    Realizado o exercício da aula 03 acerca das variáveis

---
+++ semana1/variaveis/index.js
@@ -0,0 +1,54 @@
+//Exercício 1
+//let a = 10
+//let b = 10
+
+//console.log(b)
+
+//b = 5
+//console.log(a, b)
+
+//Resposta:
+// 10
+// 10 5
+
+//-----------------------------------------------------------------------
+//Exercício 2
+
+//let a = 10
+//let b = 20
+//let c
+//c = a
+//b = c
+//a = b
+
+//console.log(a, b, c)
+
+//Resposta:
+//10 10 10
+
+//------------------------------------------------------------------------
+//Exercício 3
+
+//let p = prompt("Quantas horas você trabalha por dia?")
+//let t = prompt("Quanto você recebe por dia?")
+//alert(`Voce recebe ${t/p} por hora`)
+
+//Resposta:
+// O programa calcula qual o valor da hora diária trabalhada, pela relação horas trabalhadas/valor recebido por dia.
+// A variável p poderia ser melhor descrita com let horasTrabalhadas = prompt("Quantas horas você trabalha por dia?")
+// A variável t poderia ser melhor descrita com let valorDiária = prompt("Quanto você recebe por dia?")
+//-------------------------------------------------------------------------------------------
+
+
+let nome
+let idade
+console.log (typeof nome, typeof idade)
+
+//Resposta - Exercício 1 escrito de código
+
+// d) Uma vez que não foram atribuídos valores às variávies nome e idade, ao prosseguir com o typeof ele apresenta ambos como undefined.
+
+
+
+
+
