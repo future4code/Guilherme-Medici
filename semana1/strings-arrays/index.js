063231e7d212199de78703f594d491725a032d04
GuilhermeMedici <85452389+GuilhermeMedici@users.noreply.github.com>
2021-06-16 16:00:14 -0400

    Realizado somente os dois primeiros exercícios, irei completar posteriormente

---
+++ semana1/strings-arrays/index.js
@@ -0,0 +1,46 @@
+//EXERCÍCIO 1 - INTERPRETAÇÃO
+// let array
+// console.log('a. ', array)
+
+// array = null
+// console.log('b. ', array)
+
+// array = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+// console.log('c. ', array.length)
+
+// let i = 0
+// console.log('d. ', array[i])
+
+// array[i+1] = 19
+// console.log('e. ', array)
+
+// const valor = array[i+6]
+// console.log('f. ', valor)
+
+// a. Undefined
+// b. Null
+// c. 11
+// d. 3
+
+// e. Array com 11 itens
+// 0:3
+// 1:19
+// 2:5
+// 3:6
+// 4:7
+// 5:8
+// 6:9
+// 7:10
+// 8:11
+// 9:12
+// 10:13
+
+// f. 9
+
+//EXERCÍCIO 2 - INTERPRETAÇÃO
+
+// const frase = prompt("Digite uma frase")
+
+// console.log(frase.toUpperCase().replaceAll("A", "I"), frase.length)
+
+//RESPOSTA: Subi num ônibus em Mirrocos.
