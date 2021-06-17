11c53dbdea659a5baddfe29ba7fa72b85c8a4d52
GuilhermeMedici <85452389+GuilhermeMedici@users.noreply.github.com>
2021-06-17 15:46:44 -0400

    Exercícios realizados, restando somente a questão 3, após o envio do formulário irei terminar a 3 e realizar o PR

---
+++ semana1/funcoes/index.js
@@ -0,0 +1,82 @@
+/* EXERCÍCIO 1 - INTERPRETAÇÃO
+
+function minhaFuncao(variavel) {
+	return variavel * 5
+}
+
+console.log(minhaFuncao(2))
+console.log(minhaFuncao(10))
+
+a) Será impresso o resultado da função com base nos argumentos que forem associados ao parâmetro. No caso ele pede que seja guardado o valor da variável vezes 5. Portanto, o resultado que será impresso no console é 10 e 50.
+
+b) Nada seria impresso no console com a retirada dos console.log
+
+// EXERCÍCIO 2 - INTERPRETAÇÃO
+
+// a) A função pega o texto que o usuário inseriu, coloca em letras minúsculas e verifica/determina se o conjunto de caracteres/palavra "cenoura" está presente no texto, retornando true ou false conforme a situação.
+
+// I) true
+// II) true
+// III) true
+*/
+
+//EXERCÍCIO 1 - ESCRITA DE CÓDIGO
+
+// // A)
+// function infoMinhas () {
+//     console.log("Eu sou o Guilherme, tenho 29 anos, moro em Porto Velho/RO e sou estudante de programação.")
+// }
+
+// infoMinhas()
+
+// B)
+
+// function infoPessoas (nome, idade, cidade, profissão) {
+//     console.log(`Eu sou ${nome}, tenho ${idade} anos, moro em ${cidade} e sou ${profissão}.`)
+// }
+
+// infoPessoas("Guilherme", 23, "Porto Velho/RO", "Estudante de programação")
+
+
+//EXERCÍCIO 2 - ESCRITA DE CÓDIGO
+
+// A)
+// function somaDoisNumeros(num1, num2){
+//     const soma = num1 + num2
+//     console.log(soma)
+// }
+
+// somaDoisNumeros(5,5)
+
+// B)
+
+// function doisNumeros(num1, num2){
+//     const maiorIgual = num1 >= num2
+//     console.log(maiorIgual)
+
+// }
+
+// doisNumeros (40,20)
+
+// C)
+
+// function numPar (num1){
+//     const numPar2 = num1 % 2 === 0
+//     console.log(numPar2)
+
+// }
+
+// numPar (40)
+
+// D)
+
+// function mensagem (Frase){
+//     console.log(`A quantidade de caracteres é: ${Frase.length}`)
+//     console.log(Frase.toLowerCase())
+
+// }
+// mensagem("Olá, sejam muito bem vindos.")
+
+//EXERCÍCIO 3 - ESCRITA DE CÓDIGO
+
+
