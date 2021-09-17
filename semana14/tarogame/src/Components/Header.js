3fd32ae5e060fd4c66a8fa0ac0720807e00ff6c4
GuilhermeMedici <mailguigs@gmail.com>
2021-09-17 12:15:42 -0300

    header pronto, iniciada as tentativas de renderizar as cartas na tela

---
+++ semana14/tarogame/src/Components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { HeaderContainer } from './HeaderStyles';
+
+const Header = () => {
+
+    return (
+
+        <HeaderContainer>
+
+            <h2>TarotGame</h2>
+            <button>Start Tarot!</button>
+
+
+        </HeaderContainer>
+
+    )
+}
+
+export default Header;
---
+++ semana14/tarogame/src/Components/HeaderStyles.js
@@ -0,0 +1,10 @@
+import styled from "styled-components"
+
+export const HeaderContainer = styled.div`
+    display: flex;
+    justify-content: space-between;
+    padding-left: 15px;
+    padding-right: 15px;
+    align-items: center;
+    background-color: lightsteelblue;
+`
