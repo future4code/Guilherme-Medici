0f42485b68b5b3045aee7a09a24e5823f81019ce
GuilhermeMedici <mailguigs@gmail.com>
2021-09-21 19:04:31 -0300

    exercício 3

--- semana15/introtype/src/index.ts
+++ semana15/introtype/src/index.ts
@@ -85,4 +85,40 @@ type retorno = {
     media: number
 }
 
+//EXERCÍCIO 3
+
+// a)
+
+type postsBase = {
+    autor: string,
+    texto: string,
+  }
+
+const posts: postsBase[] = [
+    {
+      autor: "Alvo Dumbledore",
+      texto: "Não vale a pena viver sonhando e se esquecer de viver"
+    },
+    {
+      autor: "Severo Snape",
+      texto: "Menos 10 pontos para Grifinória!"
+    },
+    {
+      autor: "Hermione Granger",
+      texto: "É levi-ô-sa, não levio-sá!"
+    },
+    {
+      autor: "Dobby",
+      texto: "Dobby é um elfo livre!"
+    },
+    {
+      autor: "Lord Voldemort",
+      texto: "Avada Kedavra!"
+    }
+  ]
+
+  // b)
+
+
+
 
