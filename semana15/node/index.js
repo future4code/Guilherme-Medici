4ffbf6226a707244532a2d9ae6b0be61dae0bd97
GuilhermeMedici <mailguigs@gmail.com>
2021-09-20 22:24:11 -0300

    testando node novamente

--- semana15/node/index.js
+++ semana15/node/index.js
@@ -1,2 +1,9 @@
 const countries = require("./countries")
-console.log(countries)
+
+const name = process.argv[2]
+
+const result = countries.filter(country=>{
+    return country.name.includes(name)
+})
+
+console.table(result)
