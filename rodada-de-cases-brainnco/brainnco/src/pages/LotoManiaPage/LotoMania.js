78294a13bd9e66bac4df05459330570cd88513b9
GuilhermeMedici <mailguigs@gmail.com>
2022-01-06 01:15:16 -0300

    Finalizado Case BrainnCo

--- rodada-de-cases-brainnco/brainnco/src/pages/LotoManiaPage/LotoMania.js
+++ rodada-de-cases-brainnco/brainnco/src/pages/LotoManiaPage/LotoMania.js
@@ -1,14 +1,85 @@
-import React from "react"
+import axios from "axios"
+import { useEffect, useState } from "react"
+import { BASE_URL } from "../../constants/urls"
+import {GeneralContainer,LeftCol,RightCol,TitleWrap,Title,LogoLoterias,NumbersWrap,GameTitle,GameNumberDate,Obs,
+} from "./LotoManiaStyle"
+import logoLoterias from "../../assets/images/logo_cef.svg"
+import NumeroSorteio from "../../components/NumeroSorteio"
 
-const LotoManiaPage = () => {
+const Lotomania = () => {
+  const [relacaoConcurso, setRelacaoConcurso] = useState([])
+  const [concursoId, setConcursoId] = useState("")
+  const [concursos, setConcursos] = useState([])
 
-    return (
+  const getRelacaoConcursos = () => {
+    axios
+      .get(`${BASE_URL}/loterias-concursos`)
+      .then((res) => {
+        setRelacaoConcurso(res.data[3])
+        if (relacaoConcurso) {
+          setConcursoId(res.data[3].concursoId)
+        }
 
-        <div>
-            <h1>LotoManiaPage</h1>
-        </div>
-        
-    )
+       
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+
+  const getConcursosById = (id) => {
+    axios
+      .get(`${BASE_URL}/concursos/${id}`)
+      .then((res) => {
+        setConcursos(res.data)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+
+  useEffect(() => {
+    getRelacaoConcursos()
+  }, [])
+
+  useEffect(() => {
+    if (concursoId) getConcursosById(concursoId)
+  }, [concursoId])
+
+  const data = new Date(concursos?.data)
+  const formatedDate = data.toLocaleDateString("pt-BR", { timeZone: "UTC" })
+
+  return (
+    <GeneralContainer>
+      <LeftCol></LeftCol>
+      <TitleWrap>
+        <LogoLoterias src={logoLoterias} />
+        <Title>LOTOMANIA</Title>
+      </TitleWrap>
+      <GameTitle>
+        <p>CONCURSO  {concursos.id}</p>
+      </GameTitle>
+      <GameNumberDate>
+        <p>
+         {data && formatedDate}
+        </p>
+      </GameNumberDate>
+      <RightCol>
+        <NumbersWrap>
+          <ul>
+            {concursos.numeros &&
+              concursos.numeros.map((item) => (
+                <NumeroSorteio numbers={item} key={item} />
+              ))}
+          </ul>
+        </NumbersWrap>
+      
+        <Obs>
+          Este sorteio é meramente ilustrativo.
+        </Obs>
+      </RightCol>
+    </GeneralContainer>
+  )
 }
 
-export default LotoManiaPage
+export default Lotomania
