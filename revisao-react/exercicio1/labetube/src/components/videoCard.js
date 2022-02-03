325d03b3a1c639a0f3cfbf28c016783f3f2e0a52
GuilhermeMedici <mailguigs@gmail.com>
2022-02-03 17:35:13 -0300

    Iniciado exercicio1, muita dificuldade de entender como começar

---
+++ revisao-react/exercicio1/labetube/src/components/videoCard.js
@@ -0,0 +1,16 @@
+// import React from 'react';
+
+// export const VideoCardContainer = styled.div`
+
+// `
+
+// export const VideoCard = (props) =>  {
+
+//   return (
+//       <VideoCardContainer>
+
+//       </VideoCardContainer>
+//   )
+
+// }
+
