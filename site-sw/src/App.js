06eb92355fc9a694283f09accf0737471ae4f71d
GuilhermeMedici <mailguigs@gmail.com>
2022-02-10 21:06:29 -0300

    Etapa 2 quase finalizada, usando react router v6

--- site-sw/src/App.js
+++ site-sw/src/App.js
@@ -1,9 +1,13 @@
 import React from "react"
+import CharacterDetailPage from "./pages/CharacterDetailPage/CharacterDetailPage";
+import CharacterListPage from "./pages/CharacterListPage/CharacterListPage";
 
 function App() {
   return (
     <div>
       <h1>STAR WARS</h1>
+      <CharacterDetailPage/>
+      <CharacterListPage/>
     </div>
   )
 }
---
+++ site-sw/src/pages/CharacterDetailPage/CharacterDetailPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function CharacterDetailPage() {
+  return (
+      <div>
+          CharacterDetailPage
+      </div>
+  )
+}
+
+export default CharacterDetailPage;
